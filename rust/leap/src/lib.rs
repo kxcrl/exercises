329f35d2d9a96ba7ae29c0de480b299051cd98b7
kxcrl <jeramy.couts@gmail.com>
2015-06-09 20:57:23 -0700

    Refactor rust exercise: leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,19 +1,17 @@
-pub fn is_leap_year(year: i32) -> bool {
-    if is_exceptional_century(&year) {
-        true
-    } else {
-        is_not_century(&year) && is_divisible_by_4(&year)
-    }
-}
+pub fn is_leap_year(year: i16) -> bool {
+    // Not all centuries are leap years
+    if is_century(&year) { return is_leap_century(&year) }
 
-fn is_exceptional_century(year: &i32) -> bool {
-    year % 400 == 0
-}
-
-fn is_not_century(year: &i32) -> bool {
-    year % 100 != 0
-}
-
-fn is_divisible_by_4(year: &i32) -> bool {
+    // Years that are not centuries simply need to be divisible by 4
     year % 4 == 0
+}
+
+fn is_century(year: &i16) -> bool {
+    // A century occurs every 100 years
+    year % 100 == 0
+}
+
+fn is_leap_century(year: &i16) -> bool {
+    // Only every fourth century is a leap year
+    year % 400 == 0
 }
